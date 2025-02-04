0abeddb1a6620726e978239b558081fbe74559c3
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-04 14:02:20 +0500

    please add

---
+++ app.js
@@ -0,0 +1,28 @@
+// let number = (2)
+// let value= (3)
+// var result = number+value
+
+// document.write("sum of 2 + 3 is " + result )
+// document.write( value-number) 
+// document.write(number*value
+    
+// )
+// document.write(value/number)
+// document.write(value%number)
+// let valuee;
+// document.write(valuee)
+// valuee= (15)
+// document.write("initial value " + valuee)
+// valuee= (20)
+// document.write("value after increment is"+ valuee)
+// valuee=(valuee+7)
+// document.write("value after addition is" + valuee)
+// valuee=(14)
+// document.write("value after decrement is" + valuee)
+// valuee=(valuee%7)
+// document.write(valuee)
+// let ticketPrice = (600)
+// ticketPrice=(ticketPrice*5)
+// document.write("cost of buying 5 tickets is " + ticketPrice)
+let table;
+tablenum++
